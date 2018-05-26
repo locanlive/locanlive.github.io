edbba10416117b759af36d6640600998230e82ca
locanlive <locanlive@outlook.com>
2018-05-27 01:29:36 +0600

    again

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+function myFunction1() {
+	window.location.replace("lndex.html");
+	return true;
+}
+function myFunction2() {
+	window.location.replace("try.html");
+	return true;
+}
+function myFunction3() {
+	window.location.replace("try-again.html");
+	return true;
+}
+function myFunctionl1() {
+	window.location.replace("http://get.safedatingverified.com/?a=4267&c=59&s1=");
+	return true;
+}
+function myFunctionl2() {
+	window.location.replace("http://my.casualconnect.net/?a=4267&c=41&s1=");
+	return true;
+}
+function myFunctionl3() {
+	window.location.replace("http://click-connect.com/?a=4267&c=416&s1=");
+	return true;
+}
+function myFunctionclick() {
+	window.location.replace("http://get.safedatingverified.com/?a=4267&c=416&s1=");
+	return true;
+}
